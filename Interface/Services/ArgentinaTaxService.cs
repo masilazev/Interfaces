c484c8a7d333a78a2db42285c807d4c21ec2b998
Matheus Azevedo <matheus2002azevedo@icloud.com>
2024-09-23 09:43:34 -0300

    Created class ITaxService to work with diferents CountryTaxService

---
+++ Interface/Services/ArgentinaTaxService.cs
@@ -0,0 +1,17 @@
+﻿namespace Interface.Services
+{
+    internal class ArgentinaTaxService : ITaxService
+    {
+        public double Tax(double amount)
+        {
+            if (amount <= 100)
+            {
+                return amount * 0.4;
+            }
+            else
+            {
+                return amount * 0.3;
+            }
+        }
+    }
+}
